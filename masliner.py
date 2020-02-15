274dc893349081701ce10091a37e857a67bd8881
Rebekah Miller <RebekahLMiller@gmail.com>
2020-02-14 22:53:14 -0500

    Fixed path issues in masliner functions

--- masliner.py
+++ masliner.py
@@ -10,11 +10,16 @@ def make_experiment_description(gprdir, exp_desc):
 			NOTE: this function assumes that all files in this directory ending
 				in ".gpr" should be included in the experiment description file
 		exp_desc: the name (path) of the experiment description file to generate
-			e.g. experiment_description.txt
+			e.g. ./experiment_description.txt
 	"""
+	# Navigate to gprdir after saving current directory
+	cwd = subprocess.os.getcwd()
+	subprocess.os.chdir(gprdir)
 	# Save a sorted list of all files in gprdir that end in ".gpr"
-	files = glob.glob(gprdir + '/*.gpr')
+	files = glob.glob('*.gpr')
 	files.sort()
+	# Navigate back to original directory (allows user to input relative paths)
+	subprocess.os.chdir(cwd)
 	# Store the possible file endings (chambers) in a set to get a unique list
 	# NOTE: this assumes the file names end in '#-8.gpr'
 	chamber_set = set(filename[-7:] for filename in files)
@@ -27,7 +32,7 @@ def make_experiment_description(gprdir, exp_desc):
 			# Write the header for this chamber
 			f.write('Pbm=1\nConcentration=100\nCy3=FOO\n')
 			# Find all the file names matching this chamber and write to file
-			matches = [x for x in files if chamber in x]
+			matches = [filename for filename in files if chamber in filename]
 			for match in matches:
 				f.write(match + '\n')
 			# Add an empty line between chambers
@@ -37,7 +42,7 @@ def make_masliner_comfile(exp_desc, comfile):
 	"""Generates comfile for running masliner
 
 	Inputs:
-		exp_desc: the name of the experiment description file to use
+		exp_desc: the name (path) of the experiment description file to use
 		comfile: the name (path) of the comfile to generate
 	"""
 	# Open file for writing
@@ -54,13 +59,14 @@ def run_masliner_comfile(gprdir, comfile):
 		comfile: the ABSOLUTE path to the comfile to run
 	"""
 	# NOTE: update run_comfile_alert.pl path after -P option updated!!
+	# Navigate to gprdir after saving current directory
+	cwd = subprocess.os.getcwd()
+	subprocess.os.chdir(gprdir)
+	# Run masliner comfile
 	subprocess.os.system('perl /projectnb/siggers/data/rebekah_project/run_comfile_alert.pl -com ' +
 			comfile + ' -qsub masliner')
-
-
-
-
-
+	# Navigate back to original directory
+	subprocess.os.chdir(cwd)
 
 
 
