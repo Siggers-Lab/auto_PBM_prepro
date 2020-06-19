d12a33a28852d59f4cec368708fa24fc5e7019b2
Rebekah Miller <RebekahLMiller@gmail.com>
2020-06-19 13:36:15 -0400

    Fixed selection of masliner adjusted files

--- spatial_detrend.py
+++ spatial_detrend.py
@@ -11,29 +11,39 @@ def make_madj_gpr_list(madjgprdir, madjgprlist):
         madjgprdir: the directory where the masliner adjusted gpr files are
             NOTE: assumes that all files of the form "madj*.gpr" in this
                 directory should be used
-            madjgprlist: the path to the output list file
+        madjgprlist: the path to the output list file
     """
     # Do not overwrite madjgprlist if it already exists
     prevent_overwrite(madjgprlist)
-    
+
     # Navigate to madjgprdir after saving current working directory
     cwd = subprocess.os.getcwd()
     subprocess.os.chdir(madjgprdir)
-    
+
     # Get a list of all masliner adjusted gpr files and make sure they're sorted
     files = glob.glob("madj*.gpr")
     files = natsorted(files)
-    
-    # Navigate back to original directory (allows relative paths)
+
+    # Navigate back to original directory
     subprocess.os.chdir(cwd)
-    
-    # The last file in the list should be at the highest scan intensity
-    # Extract the main filename of this last file (everything except #-8.gpr)
-    highint = files[-1][:-7]
-    
-    # Find all filenames matching the main part of this filename
-    highintfiles = [filename for filename in files if highint in filename]
-    
+
+    # Store the possible file endings (chambers) in a set to get a unique list
+    chamberset = set(filename[-7:] for filename in files)
+
+    # Convert chamberset to a sorted list
+    chamberlist = natsorted(list(chamberset))
+
+    # Initialize a list to store the highest intensity scan for each chamber
+    highintfiles = []
+
+    # Find highest intensity scan for each chamber
+    for chamber in chamberlist:
+        # Find all files for this chamber
+        matches = [filename for filename in files if chamber in filename]
+
+        # The files are sorted, so the last one should be the highest intensity
+        highintfiles.append(matches[-1])
+
     # Write list to madjgprlist
     with open(madjgprlist, 'w') as f:
         for filename in highintfiles:
@@ -42,7 +52,7 @@ def make_madj_gpr_list(madjgprdir, madjgprlist):
 
 def make_spatial_detrend_comfile(madjgprlist, analysisfile, comfile):
     """Makes a comfile for performing spatial detrending
-    
+
     Inputs:
         madjgprlist: the path to the list of masliner adjusted gpr files
         analysisfile: the path to the analysis file
@@ -50,7 +60,7 @@ def make_spatial_detrend_comfile(madjgprlist, analysisfile, comfile):
     """
     # Do not overwrite comfile if it already exists
     prevent_overwrite(comfile)
-    
+
     # Open comfile for writing
     with open(comfile, 'w') as f:
         f.write('perl /project/siggers/perl/GENEPIX/' +
@@ -62,7 +72,7 @@ def make_spatial_detrend_comfile(madjgprlist, analysisfile, comfile):
 
 def run_spatial_detrend_comfile(comfile, madjgprdir):
     """Runs a comfile for performing spatial detrending
-    
+
     Inputs:
         comfile: the path to the comfile to run
         madjgprdir: the path to the directory in which the masliner adjusted
@@ -71,24 +81,24 @@ def run_spatial_detrend_comfile(comfile, madjgprdir):
     # Navigate to madjgprdir after saving current working directory
     cwd = subprocess.os.getcwd()
     subprocess.os.chdir(madjgprdir)
-    
+
     # Read contents of comfile as single string on one line
     with open(comfile) as f:
         comfilecont = f.read().replace('\n', ' ')
-    
+
     # Run comfile
     logging.info('qsub -sync y -P siggers -m a -cwd -N customprobes -V -b y ' +
             comfilecont)
     subprocess.run(['qsub', '-sync', 'y', '-P', 'siggers', '-m', 'a', '-cwd',
         '-N', 'customprobes', '-V', '-b', 'y', comfilecont])
-    
+
     # Navigate back to original directory
     subprocess.os.chdir(cwd)
 
 
 def spatial_detrend_wrapper(madjgprdir, analysisfile, normgprdir):
     """Runs spatial detrending on all masliner adjusted gpr files in a directory
-    
+
     Inputs:
         madjgprdir: the path to the directory containing the masliner adjusted
             gpr files to use
@@ -97,32 +107,32 @@ def spatial_detrend_wrapper(madjgprdir, analysisfile, normgprdir):
     """
     # If normgprdir already exists, abort to prevent overwrite
     prevent_overwrite(normgprdir)
-    
+
     # Get a list of all files in madjgprdir before spatial detrending
     beforefiles = glob.glob(madjgprdir + '/*')
-    
+
     # Make a file listing all the masliner adjusted gpr files in madjgprdir
     logging.info('Making a list of all masliner adjusted gpr files')
     madjgprlist = madjgprdir + '/madj_gpr.list'
     make_madj_gpr_list(madjgprdir, madjgprlist)
-    
+
     # Make a comfile for performing spatial detrending in madjgprdir
     logging.info('Making spatial detrend comfile')
     comfile = madjgprdir + '/process_custom_probes.com'
     make_spatial_detrend_comfile(madjgprlist, analysisfile, comfile)
-    
+
     # Run spatial detrending comfile
     logging.info('Running spatial detrend comfile ' +
             '(this may take a few minutes)')
     run_spatial_detrend_comfile(comfile, madjgprdir)
-    
+
     # Get a list of all files in madjgprdir after spatial detrending
     afterfiles = glob.glob(madjgprdir + '/*')
-    
+
     # Get a list of all new files in madjgprdir
     newfiles = [filename for filename in afterfiles
             if filename not in beforefiles]
-    
+
     # Make a new directory and move all new files to this directory
     subprocess.run(['mkdir', normgprdir])
     logging.info('Moving files to normalized gpr directory: ' +
